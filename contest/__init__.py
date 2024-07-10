a54c73015e13b9638e20f37cc75070902f1d4fc8
agusmart <am@eco.uc3m.es>
2024-07-10 12:23:21 +0100

    basic struct

--- contest/__init__.py
+++ contest/__init__.py
@@ -9,11 +9,15 @@ A simple Tullock game
 class C(BaseConstants):
     NAME_IN_URL = 'contest'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 1
+    NUM_ROUNDS = 2
+    ENDOWMENT = 20
 
 
 class Subsession(BaseSubsession):
-    pass
+    is_paid = models.BooleanField()
+
+    def setup(self):
+        self.is_paid = (self.round_number == 1)
 
 
 class Group(BaseGroup):
@@ -21,24 +25,39 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    endowment = models.IntegerField()
+    cost_per_ticket = models.IntegerField()
+    tickets_purchased = models.IntegerField()
+    is_winner = models.BooleanField()
+    earnings = models.IntegerField()
+
     pass
+
+
+def creating_session(subsession):
+    subsession.setup()
 
 
 # PAGES
 class Intro(Page):
     pass
 
+
 class SetupRound(WaitPage):
     pass
+
 
 class Decision(Page):
     pass
 
+
 class Waitfordecision(WaitPage):
     pass
 
+
 class Results(Page):
     pass
+
 
 class Endblock(Page):
     pass
